013c8f04d6d19dae0290ab76b6f1933c11fb930f
Fernanda Borges <fernandaborges133@gmail.com>
2025-09-13 15:58:24 -0300

    removendo o loop

--- blink.c
+++ blink.c
@@ -1,20 +1,34 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
-const uint LED_PIN = 25;
+// Define o GPIO pin onde o LED esta conectado
+const uint LED_PIN = 2;
 
 int main() 
 {
+   //inicializa o stdio e o GPIO 
     stdio_init_all();
     gpio_init(LED_PIN);
+    //Define o GPIO como saida (output)
     gpio_set_dir(LED_PIN, GPIO_OUT);
-    while (true) {
-        gpio_put(LED_PIN, 1);
-        printf("LED on\n");
-        sleep_ms(250);
-        gpio_put(LED_PIN, 0);
-        printf("LED off\n");
-        sleep_ms(250);
-    }
+   // Liga o LED
+    gpio_put(LED_PIN, 1);
+    printf("LED on\n");
+    sleep_ms(5000);  // espera 5 segundos
+    // Desliga o LED
+    gpio_put(LED_PIN, 0);
+    printf("LED off\n");
+    sleep_ms(2000);  // espera 2 segundos
     return 0;
 }
+
+ // codigo para loop infinito para ligar e desligar o LED
+    // while (true) {
+    //     gpio_put(LED_PIN, 1);
+    //     printf("LED on\n");
+    //     sleep_ms(2000);
+    //     gpio_put(LED_PIN, 0);
+    //     printf("LED off\n");
+    //     sleep_ms(2000);
+    // }
+    // return 0;
